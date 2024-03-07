2366014f639b140040a38c2d26ad5388b362d26f
Sebastian <hbhbnr@posteo.net>
2024-03-07 23:40:13 +0100

    Switch SessionStorage to ConcurrentHashMap

--- app/src/main/java/org/hbhbnr/SessionStorage.java
+++ app/src/main/java/org/hbhbnr/SessionStorage.java
@@ -3,25 +3,22 @@ package org.hbhbnr;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionStorage {
 
     private final Map<UUID, Session> sessions;
 
     public SessionStorage(HashMap<String, Session> sessions) {
-        this.sessions = new HashMap<>();
+        this.sessions = new ConcurrentHashMap<>();
     }
 
     public Session getSession(final Session session) {
         return sessions.put(session.getID(), session);
     }
 
-    public Session getSession(final UUID sessionID) {
-        return sessions.get(sessionID);
-    }
-
-    public boolean sessionsExists(final UUID sessionID) {
-        return sessions.containsKey(sessionID);
+    public Session getSessionOrNull(final UUID sessionID) {
+        return sessions.getOrDefault(sessionID, null);
     }
 
 }
